0ae3578459b0548d9c6298b2c62587ee8362b611
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-08-16 13:28:56 +0100

    generate secretToken stored in config.js & 02 test

--- __tests__/GitHubSDK.test.js
+++ __tests__/GitHubSDK.test.js
@@ -1,8 +1,16 @@
-const { expect } = require("@jest/globals");
+const { expect, it } = require("@jest/globals");
 import GitHubSDK from "./../src/GitHubSDK";
 
 describe("GitHubDSK", () => {
     it("check if class GitHubDSK exist", () => {
         const gh = new GitHubSDK();
+    });
+    it("should have set username and secret token when creating an instance", () => {
+        const username = "axseinga";
+        const secretToken = "ghp_s374example";
+        const gh = new GitHubSDK(username, secretToken);
+
+        expect(gh.username).toBe(username);
+        expect(gh.secretToken).toBe(secretToken);
     });
 });
