2b6dd0a0b3deca37658538502b6191ffe2939162
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-08-16 13:07:13 +0100

    test 01 if class exist

---
+++ __tests__/GitHubSDK.test.js
@@ -0,0 +1,7 @@
+const GitHubSDK = require("./../src/GitHubSDK");
+
+describe("GitHubDSK", () => {
+    it("check if class GitHubDSK exist", () => {
+        const gh = new GitHubSDK();
+    });
+});
